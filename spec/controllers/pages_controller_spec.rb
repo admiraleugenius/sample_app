f8d32667d7d5cd5687951b78d4bafa00f54b04c1
Matthias Menn <admiraleugenius@net2000.ch>
2010-01-14 09:48:53 +0100

    "added Help page"

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 describe PagesController do 
 integrate_views
+
+before(:each) do
+@base_title="Ruby on Rails Tutorial Sample App"
+end
+
 describe "GET 'home'" do
 it "should be successful" do
 get 'home'
@@ -8,7 +13,7 @@ response.should be_success
 end
 it "should have the right title" do
 get 'home'
-response.should have_tag("title","Ruby on Rails Tutorial Sample App | Home")
+response.should have_tag("title",@base_title + " | Home")
 end
 end
 describe "GET 'contact'" do it "should be successful" do
@@ -17,7 +22,7 @@ response.should be_success
 end
 it "should have the right title" do
 get 'contact'
-response.should have_tag("title","Ruby on Rails Tutorial Sample App | Contact")
+response.should have_tag("title",@base_title + " | Contact")
 end
 end
 describe "GET 'about'" do it "should be successful" do
@@ -26,8 +31,21 @@ response.should be_success
 end
 it "should have the right title" do
 get 'about'
-response.should have_tag("title","Ruby on Rails Tutorial Sample App | About")
+response.should have_tag("title",@base_title + " | About")
 end
+end
+
+describe "GET 'help'" do
+it "should be successful" do
+get 'help'
+response.should be_success
+end
+
+it "should have the right title" do
+get 'help'
+response.should have_tag("title",@base_title + " | Help")
+end
+
 end
 end
 
